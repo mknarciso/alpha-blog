da394368c6eaa7cba500f56db7c54b7ed8238fce
mknarciso@hotmail.com <murilokn@gmail.com>
2016-01-09 19:51:09 +0000

    method and ready to go

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
 
     def new
         @article = Article.new
     end
     
     def edit
-        
-        @article = Article.find(params[:id])
-        
     end
     
     def create
@@ -21,7 +20,6 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -31,9 +29,6 @@ class ArticlesController < ApplicationController
     end
     
     def show 
-        
-       @article = Article.find(params[:id])
-        
     end
     
     def index
@@ -43,12 +38,9 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article deleted"
         redirect_to articles_path
-        
     end
     
     private
@@ -56,6 +48,9 @@ class ArticlesController < ApplicationController
             params.require(:article).permit(:title, :description)    
         end
         
+        def set_article
+            @article = Article.find(params[:id])
+        end
 
     
 end
