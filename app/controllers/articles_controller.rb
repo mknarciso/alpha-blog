a0d7f5324519681329fb0de2d823bfe2e18b4328
mknarciso@hotmail.com <murilokn@gmail.com>
2016-01-09 19:44:19 +0000

    Delete and _form and _messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,6 +42,15 @@ class ArticlesController < ApplicationController
         
     end
     
+    def destroy
+        
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article deleted"
+        redirect_to articles_path
+        
+    end
+    
     private
         def article_params
             params.require(:article).permit(:title, :description)    
